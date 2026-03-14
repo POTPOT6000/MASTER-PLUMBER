667f2b2c2d4b688ed59118a63e8f9e5704e3a446
POTPOT6000 <jaytristanmedallon@gmail.com>
2026-03-14 15:44:02 +0800

    Add files via upload

---
+++ symbols.js
@@ -0,0 +1,124 @@
+export const symbolsDB = {
+    1: [
+  { "q": "What is this symbol?", "choices": ["Sitz Bath", "Shower Stall", "Corner Bathtub", "Recessed Bathtub"], "a": 2, "img": "images/corner_bathtub.png" },
+  { "q": "What is this symbol?", "choices": ["Drinking Fountain (Recessed)", "Drinking Fountain (Projecting-Type)", "Pedestal Lavatory", "Drinking Fountain (Semi-Recessed)"], "a": 1, "img": "images/drinking_fountain_projecting.png" },
+  { "q": "What is this symbol?", "choices": ["Sitz Bath", "Recessed Bathtub", "Corner Bathtub", "Shower Stall"], "a": 1, "img": "images/recessed_bathtub.png" },
+  { "q": "What is this symbol?", "choices": ["Water Heater", "Grease Separator", "Hot Water Tank", "Oil Separator"], "a": 2, "img": "images/hot_water_tank.png" },
+  { "q": "What is this symbol?", "choices": ["Bidet", "Pedestal Lavatory", "Wall Lavatory", "Sitz Bath"], "a": 3, "img": "images/sitz_bath.png" },
+  { "q": "What is this symbol?", "choices": ["Water Heater", "Hot Water Tank", "Service Sink", "Grease Separator"], "a": 0, "img": "images/water_heater.png" },
+  { "q": "What is this symbol?", "choices": ["Pedestal Lavatory", "Wall Lavatory", "Bidet", "Corner Lavatory"], "a": 2, "img": "images/bidet.png" },
+  { "q": "What is this symbol?", "choices": ["Hose Bibb", "Meter", "Gas Outlet", "Vacuum Outlet"], "a": 1, "img": "images/meter.png" },
+  { "q": "What is this symbol?", "choices": ["Overhead Gang Shower", "Corner Bathtub", "Sitz Bath", "Shower Stall"], "a": 3, "img": "images/shower_stall.png" },
+  { "q": "What is this symbol?", "choices": ["Hose Bibb", "Hose Rack", "Shower Head", "Gas Outlet"], "a": 1, "img": "images/hose_rack.png" },
+  { "q": "What is this symbol?", "choices": ["Hose Bibb", "Shower Stall", "Shower Head", "Gas Outlet"], "a": 2, "img": "images/shower_head.png" },
+  { "q": "What is this symbol?", "choices": ["Gas Outlet", "Vacuum Outlet", "Hose Bibb", "Meter"], "a": 2, "img": "images/hose_bibb.png" },
+  { "q": "What is this symbol?", "choices": ["Hose Bibb", "Gas Outlet", "Vacuum Outlet", "Meter"], "a": 1, "img": "images/gas_outlet.png" },
+  { "q": "What is this symbol?", "choices": ["Gas Outlet", "Hose Bibb", "Vacuum Outlet", "Drain"], "a": 2, "img": "images/vacuum_outlet.png" },
+
+  
+  { "q": "What is this symbol?", "choices": ["Shower Stall", "Shower Head", "Overhead Gang Shower", "Service Sink"], "a": 2, "img": "images/overhead_gang_shower.png" },
+  { "q": "What is this symbol?", "choices": ["Wall Lavatory", "Pedestal Lavatory", "Corner Lavatory", "Handicapped Lavatory"], "a": 1, "img": "images/pedestal_lavatory.png" },
+  { "q": "What is this symbol?", "choices": ["Pedestal Lavatory", "Wall Lavatory", "Corner Lavatory", "Handicapped Lavatory"], "a": 1, "img": "images/wall_lavatory.png" },
+  { "q": "What is this symbol?", "choices": ["Wall Lavatory", "Corner Lavatory", "Pedestal Lavatory", "Handicapped Lavatory"], "a": 1, "img": "images/corner_lavatory.png" },
+  { "q": "What is this symbol?", "choices": ["Floor Drain with Backwater Valve", "Garage Drain", "Drain", "Roof Sump"], "a": 2, "img": "images/drain.png" },
+  { "q": "What is this symbol?", "choices": ["Dental Lavatory", "Pedestal Lavatory", "Handicapped Lavatory", "Wall Lavatory"], "a": 2, "img": "images/handicapped_lavatory.png" },
+  { "q": "What is this symbol?", "choices": ["Handicapped Lavatory", "Dental Lavatory", "Pedestal Lavatory", "Wall Lavatory"], "a": 1, "img": "images/dental_lavatory.png" },
+  { "q": "What is this symbol?", "choices": ["Kitchen Sink, R & L Drain Board", "Standard Kitchen Sink", "Kitchen Sink, LH Drain Board", "Combination Sink & Dishwasher"], "a": 1, "img": "images/standard_kitchen_sink.png" },
+  { "q": "What is this symbol?", "choices": ["Kitchen Sink, LH Drain Board", "Kitchen Sink, R & L Drain Board", "Standard Kitchen Sink", "Combination Sink & Dishwasher"], "a": 1, "img": "images/kitchen_sink_rl.png" },
+  { "q": "What is this symbol?", "choices": ["Kitchen Sink, LH Drain Board", "Kitchen Sink, R & L Drain Board", "Standard Kitchen Sink", "Combination Sink & Dishwasher"], "a": 0, "img": "images/kitchen_sink_lh.png" },
+  { "q": "What is this symbol?", "choices": ["Combination Sink & Laundry Tray", "Combination Sink & Dishwasher", "Standard Kitchen Sink", "Service Sink"], "a": 1, "img": "images/combination_sink_dishwasher.png" },
+  { "q": "What is this symbol?", "choices": ["Combination Sink & Dishwasher", "Standard Kitchen Sink", "Combination Sink & Laundry Tray", "Service Sink"], "a": 2, "img": "images/combination_sink_laundry.png" },
+  { "q": "What is this symbol?", "choices": ["Wash Sink", "Service Sink", "Laundry Tray (Single)", "Laundry Tray (Double)"], "a": 1, "img": "images/service_sink.png" },
+  { "q": "What is this symbol?", "choices": ["Wash Sink", "Service Sink", "Wash Sink (Wall-Type)", "Laundry Tray"], "a": 2, "img": "images/wash_sink_wall.png" },
+  { "q": "What is this symbol?", "choices": ["Wash Sink (Wall-Type)", "Service Sink", "Wash Sink", "Laundry Tray"], "a": 2, "img": "images/wash_sink.png" },
+  { "q": "What is this symbol?", "choices": ["Laundry Tray (Double)", "Laundry Tray (Single)", "Wash Sink", "Service Sink"], "a": 1, "img": "images/laundry_tray_single.png" },
+  { "q": "What is this symbol?", "choices": ["Laundry Tray (Single)", "Laundry Tray (Double)", "Wash Sink", "Service Sink"], "a": 1, "img": "images/laundry_tray_double.png" },
+  { "q": "What is this symbol?", "choices": ["Oil Separator", "Water Heater", "Grease Separator", "Hot Water Tank"], "a": 2, "img": "images/grease_separator.png" },
+  { "q": "What is this symbol?", "choices": ["Grease Separator", "Oil Separator", "Water Heater", "Hot Water Tank"], "a": 1, "img": "images/oil_separator.png" },
+  { "q": "What is this symbol?", "choices": ["Drain", "Cleanout", "Garage Drain", "Roof Sump"], "a": 1, "img": "images/cleanout.png" },
+  { "q": "What is this symbol?", "choices": ["Floor Drain with Backwater Valve", "Garage Drain", "Roof Sump", "Drain"], "a": 1, "img": "images/garage_drain.png" },
+  { "q": "What is this symbol?", "choices": ["Garage Drain", "Roof Sump", "Floor Drain with Backwater Valve", "Drain"], "a": 2, "img": "images/floor_drain_backwater.png" },
+  { "q": "What is this symbol?", "choices": ["Floor Drain with Backwater Valve", "Roof Sump", "Garage Drain", "Drain"], "a": 1, "img": "images/roof_sump.png" },
+
+  
+  { "q": "What is this symbol?", "choices": ["Soil and Waste, Below Grade", "Soil and Waste, Above Grade", "Vent", "Cold Water"], "a": 1, "img": "images/soil_waste_above.png" },
+  { "q": "What is this symbol?", "choices": ["Soil and Waste, Below Grade", "Soil and Waste, Above Grade", "Vent", "Cold Water"], "a": 0, "img": "images/soil_waste_below.png" },
+  { "q": "What is this symbol?", "choices": ["Soil and Waste, Above Grade", "Vent", "Soil and Waste, Below Grade", "Cold Water"], "a": 1, "img": "images/vent.png" },
+  { "q": "What is this symbol?", "choices": ["Hot Water", "Cold Water", "Hot Water Return", "Fire Line"], "a": 1, "img": "images/cold_water.png" },
+  { "q": "What is this symbol?", "choices": ["Cold Water", "Hot Water", "Hot Water Return", "Fire Line"], "a": 1, "img": "images/hot_water.png" },
+  { "q": "What is this symbol?", "choices": ["Cold Water", "Hot Water", "Hot Water Return", "Fire Line"], "a": 2, "img": "images/hot_water_return.png" },
+  { "q": "What is this symbol?", "choices": ["Gas Line", "Acid Waste", "Fire Line", "Drinking Water Supply"], "a": 2, "img": "images/fire_line.png" },
+  { "q": "What is this symbol?", "choices": ["Fire Line", "Gas Line", "Acid Waste", "Drinking Water Supply"], "a": 1, "img": "images/gas_line.png" },
+  { "q": "What is this symbol?", "choices": ["Gas Line", "Acid Waste", "Fire Line", "Drinking Water Supply"], "a": 1, "img": "images/acid_waste.png" },
+  { "q": "What is this symbol?", "choices": ["Drinking Water Return", "Vacuum Cleaning", "Drinking Water Supply", "Compressed Air"], "a": 2, "img": "images/drinking_water_supply.png" },
+  { "q": "What is this symbol?", "choices": ["Drinking Water Supply", "Drinking Water Return", "Vacuum Cleaning", "Compressed Air"], "a": 1, "img": "images/drinking_water_return.png" },
+  { "q": "What is this symbol?", "choices": ["Drinking Water Return", "Drinking Water Supply", "Vacuum Cleaning", "Compressed Air"], "a": 2, "img": "images/vacuum_cleaning.png" },
+  { "q": "What is this symbol?", "choices": ["Vacuum Cleaning", "Compressed Air", "Drinking Water Return", "Drinking Water Supply"], "a": 1, "img": "images/compressed_air.png" },
+
+  
+  { "q": "What is this symbol?", "choices": ["Elbow-45°", "Elbow-Turned Up", "Elbow-90°", "Elbow-Turned Down"], "a": 2, "img": "images/elbow_90.png" },
+  { "q": "What is this symbol?", "choices": ["Elbow-45°", "Elbow-90°", "Elbow-Turned Up", "Elbow-Turned Down"], "a": 0, "img": "images/elbow_45.png" },
+  { "q": "What is this symbol?", "choices": ["Elbow-Turned Down", "Elbow-90°", "Elbow-45°", "Elbow-Turned Up"], "a": 3, "img": "images/elbow_turned_up.png" },
+  { "q": "What is this symbol?", "choices": ["Elbow-Turned Up", "Elbow-90°", "Elbow-45°", "Elbow-Turned Down"], "a": 3, "img": "images/elbow_turned_down.png" },
+
+  
+  { "q": "What is this symbol?", "choices": ["Base Elbow", "Double Branch Elbow", "Elbow-Long Radius", "Reducing Elbow"], "a": 2, "img": "images/elbow_long_radius.png" },
+  { "q": "What is this symbol?", "choices": ["Side Outlet Elbow-Outlet Up", "Double Branch Elbow", "Reducing Elbow", "Side Outlet Elbow-Outlet Down"], "a": 3, "img": "images/side_outlet_elbow_down.png" },
+  { "q": "What is this symbol?", "choices": ["Side Outlet Elbow-Outlet Up", "Side Outlet Elbow-Outlet Down", "Double Branch Elbow", "Reducing Elbow"], "a": 0, "img": "images/side_outlet_elbow_up.png" },
+  { "q": "What is this symbol?", "choices": ["Elbow-Long Radius", "Base Elbow", "Double Branch Elbow", "Reducing Elbow"], "a": 1, "img": "images/base_elbow.png" },
+  { "q": "What is this symbol?", "choices": ["Reducing Elbow", "Side Outlet Elbow", "Double Branch Elbow", "Base Elbow"], "a": 2, "img": "images/double_branch_elbow.png" },
+  { "q": "What is this symbol?", "choices": ["Double Sweep Tee", "Tee", "Single Sweep Tee", "Tee-Outlet Up"], "a": 2, "img": "images/single_sweep_tee.png" },
+  { "q": "What is this symbol?", "choices": ["Single Sweep Tee", "Double Sweep Tee", "Tee", "Tee-Outlet Up"], "a": 1, "img": "images/double_sweep_tee.png" },
+  { "q": "What is this symbol?", "choices": ["Double Branch Elbow", "Base Elbow", "Reducing Elbow", "Side Outlet Elbow"], "a": 2, "img": "images/reducing_elbow.png" },
+  { "q": "What is this symbol?", "choices": ["Tee-Outlet Down", "Single Sweep Tee", "Tee-Outlet Up", "Tee"], "a": 2, "img": "images/tee_outlet_up.png" },
+  { "q": "What is this symbol?", "choices": ["Tee-Outlet Up", "Tee-Outlet Down", "Side Outlet Tee", "Tee"], "a": 3, "img": "images/tee.png" },
+  { "q": "What is this symbol?", "choices": ["Tee-Outlet Up", "Tee-Outlet Down", "Tee", "Side Outlet Tee"], "a": 1, "img": "images/tee_outlet_down.png" },
+  { "q": "What is this symbol?", "choices": ["Side Outlet Tee-Outlet Down", "Tee", "Side Outlet Tee-Outlet Up", "Cross"], "a": 2, "img": "images/side_outlet_tee_up.png" },
+  { "q": "What is this symbol?", "choices": ["Side Outlet Tee-Outlet Up", "Side Outlet Tee-Outlet Down", "Tee", "Cross"], "a": 1, "img": "images/side_outlet_tee_down.png" },
+  { "q": "What is this symbol?", "choices": ["Tee", "Lateral", "Cross", "Reducing Elbow"], "a": 2, "img": "images/cross.png" },
+  { "q": "What is this symbol?", "choices": ["Eccentric Reducer", "Expansion Joint", "Lateral", "Concentric Reducer"], "a": 3, "img": "images/concentric_reducer.png" },
+  { "q": "What is this symbol?", "choices": ["Concentric Reducer", "Eccentric Reducer", "Expansion Joint", "Lateral"], "a": 1, "img": "images/eccentric_reducer.png" },
+  { "q": "What is this symbol?", "choices": ["Cross", "Tee", "Reducing Elbow", "Lateral"], "a": 3, "img": "images/lateral.png" },
+  { "q": "What is this symbol?", "choices": ["Concentric Reducer", "Expansion Joint", "Eccentric Reducer", "Lateral"], "a": 1, "img": "images/expansion_joint.png" },
+  { "q": "What is this symbol?", "choices": ["Water Closet (Integral Tank)", "Water Closet (Flush Valve, Floor Outlet)", "Water Closet (Tank-Type)", "Water Closet (Flush Valve, Wall-Hung)"], "a": 2, "img": "images/wc_tank_type.png" },
+  { "q": "What is this symbol?", "choices": ["Water Closet (Tank-Type)", "Water Closet (Integral Tank)", "Water Closet (Flush Valve, Floor Outlet)", "Water Closet (Flush Valve, Wall-Hung)"], "a": 1, "img": "images/wc_integral_tank.png" },
+  { "q": "What is this symbol?", "choices": ["Water Closet (Flush Valve, Wall-Hung)", "Water Closet (Tank-Type)", "Water Closet (Integral Tank)", "Water Closet (Flush Valve, Floor Outlet)"], "a": 3, "img": "images/wc_flush_valve_floor.png" },
+  { "q": "What is this symbol?", "choices": ["Water Closet (Flush Valve, Floor Outlet)", "Water Closet (Tank-Type)", "Water Closet (Flush Valve, Wall-Hung)", "Water Closet (Integral Tank)"], "a": 2, "img": "images/wc_flush_valve_wall.png" },
+  { "q": "What is this symbol?", "choices": ["Urinal (Stall)", "Urinal (Trough-Type)", "Urinal (Wall-Hung)", "Water Closet"], "a": 2, "img": "images/urinal_wall_hung.png" },
+  { "q": "What is this symbol?", "choices": ["Urinal (Wall-Hung)", "Urinal (Stall)", "Urinal (Trough-Type)", "Water Closet"], "a": 1, "img": "images/urinal_stall.png" },
+  { "q": "What is this symbol?", "choices": ["Urinal (Wall-Hung)", "Urinal (Stall)", "Urinal (Trough-Type)", "Water Closet"], "a": 2, "img": "images/urinal_trough.png" },
+  { "q": "What is this symbol?", "choices": ["Drinking Fountain (Projecting-Type)", "Drinking Fountain (Semi-Recessed)", "Pedestal Lavatory", "Drinking Fountain (Recessed)"], "a": 3, "img": "images/drinking_fountain_recessed.png" },
+  { "q": "What is this symbol?", "choices": ["Drinking Fountain (Recessed)", "Drinking Fountain (Projecting-Type)", "Drinking Fountain (Semi-Recessed)", "Pedestal Lavatory"], "a": 2, "img": "images/drinking_fountain_semirecessed.png" },
+
+  
+  { "q": "What is this symbol?", "choices": ["Globe Valve", "Check Valve", "Gate Valve", "Float Valve"], "a": 2, "img": "images/gate_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Angle Globe Valve", "Angle Gate Valve", "Check Valve", "Globe Valve"], "a": 3, "img": "images/globe_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Globe Valve", "Angle Gate Valve", "Angle Globe Valve", "Check Valve"], "a": 2, "img": "images/angle_globe_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Angle Globe Valve", "Gate Valve", "Angle Gate Valve", "Check Valve"], "a": 2, "img": "images/angle_gate_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Angle Check Valve", "Stop Cock", "Check Valve", "Safety Valve"], "a": 2, "img": "images/check_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Check Valve", "Stop Cock", "Angle Check Valve", "Safety Valve"], "a": 2, "img": "images/angle_check_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Check Valve", "Safety Valve", "Stop Cock", "Globe Valve"], "a": 2, "img": "images/stop_cock.png" },
+  { "q": "What is this symbol?", "choices": ["Float Valve", "Quick-Opening Valve", "Safety Valve", "Globe Valve"], "a": 2, "img": "images/safety_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Float Valve", "Safety Valve", "Quick-Opening Valve", "Globe Valve"], "a": 2, "img": "images/quick_opening_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Safety Valve", "Quick-Opening Valve", "Float Valve", "Motor-Operated Gate Valve"], "a": 2, "img": "images/float_valve.png" },
+  { "q": "What is this symbol?", "choices": ["Gate Valve", "Motor-Operated Gate Valve", "Globe Valve", "Check Valve"], "a": 1, "img": "images/motor_operated_gate_valve.png" }
+],
+    2: [
+        { q: "What does a dashed line usually represent in a plumbing plan?", choices: ["Cold water line", "Vent pipe", "Hot water line", "Gas line"], a: 1 },
+        { q: "Which symbol typically represents a floor drain?", choices: ["A circle with an 'FD'", "A square with a cross", "A solid triangle", "A wavy line"], a: 0 }
+    ],
+    3: [
+        { q: "What does a dashed line usually represent in a plumbing plan?", choices: ["Cold water line", "Vent pipe", "Hot water line", "Gas line"], a: 1 },
+        { q: "Which symbol typically represents a floor drain?", choices: ["A circle with an 'FD'", "A square with a cross", "A solid triangle", "A wavy line"], a: 0 }
+    ],
+    4: [
+        { q: "What does a dashed line usually represent in a plumbing plan?", choices: ["Cold water line", "Vent pipe", "Hot water line", "Gas line"], a: 1 },
+        { q: "Which symbol typically represents a floor drain?", choices: ["A circle with an 'FD'", "A square with a cross", "A solid triangle", "A wavy line"], a: 0 }
+    ]
+
+
+
+
+};
+
+
